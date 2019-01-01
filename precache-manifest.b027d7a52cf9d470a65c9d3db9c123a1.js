ca37a476810bd0072e7f44b15e21aaa3fa9b04c0
zmiles17 <zgm_striker17@hotmail.com>
2018-12-31 20:24:29 -0500

    Updates

---
+++ precache-manifest.b027d7a52cf9d470a65c9d3db9c123a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "71cff4495fe45cdae67d",
+    "url": "/recipe-app/static/js/runtime~main.71cff449.js"
+  },
+  {
+    "revision": "1b4368700584d432b56c",
+    "url": "/recipe-app/static/js/main.1b436870.chunk.js"
+  },
+  {
+    "revision": "a44f0c0078e70b51841d",
+    "url": "/recipe-app/static/js/1.a44f0c00.chunk.js"
+  },
+  {
+    "revision": "1b4368700584d432b56c",
+    "url": "/recipe-app/static/css/main.0ef677e7.chunk.css"
+  },
+  {
+    "revision": "2977811bf821f14540ffbdddf165ca38",
+    "url": "/recipe-app/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/recipe-app/precache-manifest.b027d7a52cf9d470a65c9d3db9c123a1.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/recipe-app/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
